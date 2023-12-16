d9e314df4628d1de796d85a10abd021cbf4c66e7
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2023-12-16 22:02:33 +0530

    Example refrence added for just tem... server started in backend and database connected.

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,5 @@ $server.use(express.urlencoded({ extended: true }))
 require("dotenv").config()
 require("./configs/databaseConnection")(process.env.DATABASE)
 
-
 const PORT = process.env.PORT
 $server.listen(PORT, () => console.log(`Server Started: http://localhost:${PORT}`))
